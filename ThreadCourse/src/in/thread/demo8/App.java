5dbd8678e31f2bfcd8c891ca130011c15f101d5d
Vivek <Vivek@Vivek-PC>
2014-05-03 16:18:02 +0530

    Implemented executor service

--- ThreadCourse/src/in/thread/demo8/App.java
+++ ThreadCourse/src/in/thread/demo8/App.java
@@ -1,5 +1,50 @@
 package in.thread.demo8;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Runner implements Runnable {
+	private int id;
+
+	public Runner(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public void run() {
+		System.out.println(id + " Started");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(id + " Completed");
+	}
+
+}
+
 public class App {
+	public static void main(String[] args) {
+		new App().runTheApp();
+	}
+
+	private void runTheApp() {
+		ExecutorService executorService = Executors.newFixedThreadPool(2);
+		for (int i = 0; i < 5; i++) {
+			executorService.submit(new Runner(i));
+		}
+		System.out.println("All Tasks Submitted");
+		executorService.shutdown();
+		
+		try {
+			executorService.awaitTermination(1, TimeUnit.DAYS);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("All Tasks Completed");
+	}
 
 }
