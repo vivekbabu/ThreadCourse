57891a122eb5e2efdf16991661e63842d4ae8a66
vivekbabu <vivek.babu@gmail.com>
2015-07-25 23:19:08 +0530

    Executor Service Added graceful shutdown

--- ThreadCourse/src/in/thread/executorservice/ExecutorService.java
+++ ThreadCourse/src/in/thread/executorservice/ExecutorService.java
@@ -8,12 +8,22 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.Semaphore;
 
 public class ExecutorService {
-
+	int threadPoolSize = 0;
 	@SuppressWarnings("rawtypes")
 	BlockingQueue<CallableRecord> queue = new LinkedBlockingQueue<CallableRecord>();
 	List<Thread> threads = new ArrayList<Thread>();
-	
+
+	Semaphore safeToShutDownSemaphore;
+	private boolean takeMoreJobs = true;
+
 	public ExecutorService(int threadPoolSize) {
+		this.threadPoolSize = threadPoolSize;
+		safeToShutDownSemaphore = new Semaphore(threadPoolSize);
+		initializeThreadPool(threadPoolSize);
+
+	}
+
+	private void initializeThreadPool(int threadPoolSize) {
 		for (int i = 0; i < threadPoolSize; i++) {
 			Thread thread = new Thread() {
 				@SuppressWarnings({ "unchecked", "rawtypes" })
@@ -22,8 +32,9 @@ public class ExecutorService {
 						CallableRecord callableRecord = null;
 						try {
 							callableRecord = queue.take();
+							safeToShutDownSemaphore.acquire();
 						} catch (InterruptedException e) {
-							//e.printStackTrace();
+							// e.printStackTrace();
 							System.out.println("Shutting down thread");
 							break;
 						}
@@ -35,6 +46,7 @@ public class ExecutorService {
 						}
 						callableRecord.getFuture().setResult(call);
 						callableRecord.getFuture().getSemaphore().release();
+						safeToShutDownSemaphore.release();
 					}
 
 				};
@@ -42,28 +54,56 @@ public class ExecutorService {
 			threads.add(thread);
 			thread.start();
 		}
-
 	}
 
-	public <T> Future<T> submit(Callable<T> task) {
-		Semaphore semaphore = new Semaphore(0);
-		CallableRecord<T> callableRecord = new CallableRecord<T>();
-		callableRecord.setCallable(task);
+	public <T> Future<T> submit(Callable<T> task) throws Exception {
+		if (takeMoreJobs) {
+			Semaphore semaphore = new Semaphore(0);
+			CallableRecord<T> callableRecord = new CallableRecord<T>();
+			callableRecord.setCallable(task);
 
-		Future<T> future = new Future<T>();
-		future.setSemaphore(semaphore);
+			Future<T> future = new Future<T>();
+			future.setSemaphore(semaphore);
 
-		callableRecord.setFuture(future);
+			callableRecord.setFuture(future);
 
-		queue.offer(callableRecord);
-
-		return future;
-	}
-	
-	public void shutdown() {
-		for (Thread thread : threads) {
-			thread.interrupt();
+			queue.offer(callableRecord);
+			return future;
 		}
+
+		else {
+			throw new Exception(
+					"Service already shutdown. No more jobs will be taken. Already submitted jobs will be completed");
+		}
+
 	}
-	
+
+	public void shutdown() {
+		takeMoreJobs = false;
+		Thread thread = new Thread() {
+			public void run() {
+
+				while (true) {
+					try {
+						safeToShutDownSemaphore.acquire(threadPoolSize);
+						if (queue.isEmpty()) {
+							break;
+						} else {
+							safeToShutDownSemaphore.release(threadPoolSize);
+							Thread.sleep(10000);
+						}
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+
+				for (Thread thread : threads) {
+					thread.interrupt();
+				}
+				System.out.println("Exector service shutdown");
+			};
+		};
+		thread.start();
+		System.out.println("Exector service shutdown called");
+	}
 }
--- ThreadCourse/src/in/thread/executorservice/ExecutorServiceClient.java
+++ ThreadCourse/src/in/thread/executorservice/ExecutorServiceClient.java
@@ -6,11 +6,11 @@ import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class ExecutorServiceClient {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		ExecutorService executorService = new ExecutorService(2);
 		List<Future<Long>> futures = new ArrayList<>();
 		final Random random = new Random();
-		for (long i = 1; i <= 5; i++) {
+		for (long i = 1; i <= 20; i++) {
 			CallableWithID<Long> callable = new CallableWithID<Long>(i) {
 
 				@Override
@@ -22,20 +22,43 @@ public class ExecutorServiceClient {
 				}
 			};
 			System.out.println("Submitted job " + i);
-			Future<Long> future = executorService.submit(callable);
-			futures.add(future);
-		}
-		
-		for (int i = 1; i <= 5; i++) {
+			Future<Long> future;
 			try {
-				Long value = futures.get(i-1).get();
+				future = executorService.submit(callable);
+				futures.add(future);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+
+		}
+
+		executorService.shutdown();
+
+		for (int i = 0; i < 10; i++) {
+			Thread.sleep(2000);
+			try {
+				executorService.submit(new Callable<String>() {
+					@Override
+					public String call() throws Exception {
+						System.out.println("Run");
+						return "";
+					}
+				});
+				
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
+		for (int i = 1; i <= 20; i++) {
+			try {
+				Long value = futures.get(i - 1).get();
 				System.out.println(value + " returned to client by job " + i);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 		System.out.println("All jobs done");
-		executorService.shutdown();
-		System.out.println("Executor Service Shutdown");
+
 	}
 }
